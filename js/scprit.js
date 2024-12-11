8b009697f530a3b398cc9981bf50dee36e653035
Jonselorm <johnsongone15@gmail.com>
2024-12-11 09:32:21 +0000

    moving on to responsiveness

---
+++ js/scprit.js
@@ -0,0 +1,30 @@
+document.querySelectorAll("image-slider img"). forEach(images =>{
+    images.onclick =() => {
+        var src = images.getAttribute("src");
+        document.queryselector(".main-home-image").src = src;
+
+    };
+});
+
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween:20,
+    pagination: {
+        el: ".Swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+    grabCursor: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+    },
+});
