e7b427f83a832894b94659039594ad976766851b
Jonselorm <johnsongone15@gmail.com>
2024-12-17 22:23:18 +0000

    fully responsive

--- js/scprit.js
+++ js/scprit.js
@@ -1,3 +1,26 @@
+// humburger dey here
+function  showsidebar(){
+    const sidebar=document.querySelector(".sidebar");
+    sidebar.style.display="flex";
+}
+function hidesidebar(){
+    const hidesidebar=document.querySelector(".sidebar");
+    hidesidebar.style.display="none";
+}
+ 
+
+const navoptions = document.querySelectorAll(".navoptions"); 
+navoptions.forEach((navoptions) => {
+  navoptions.addEventListener("click", () => {
+    hideSidebar(); // Hide the sidebar on click
+  });
+});
+
+
+//humburger dey end
+
+
+
 document.querySelectorAll("image-slider img"). forEach(images =>{
     images.onclick =() => {
         var src = images.getAttribute("src");
